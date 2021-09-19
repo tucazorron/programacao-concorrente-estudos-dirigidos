0ec05017efe6b3b3b65f6bb55cbceda321adb981
artur zorron <tucazorron@gmail.com>
2021-09-19 20:09:44 -0300

    fix threads creation

--- threads-creation.c
+++ threads-creation.c
@@ -5,18 +5,34 @@
 
 #define N 10
 
+pthread_mutex_t threads = PTHREAD_MUTEX_INITIALIZER;
+pthread_t a[N];
 int counter = 0;
+int flag = 1;
 
-void *pthread_func(void *arg)
+void *pthread_func(void *id)
 {
-    int id = *((int *)arg);
-    printf("Criou uma pthread com id = %d\n", id);
+    printf("\nTHREAD  =  %02d | CRIADA\n\n", *(int *)(id));
+
+    while (flag)
+    {
+        pthread_mutex_lock(&threads);
+        if (counter < 1000)
+        {
+            counter++;
+            printf("COUNTER =  %04d | THREAD = %02d\n", counter, *(int *)(id));
+        }
+        else
+        {
+            flag = 0;
+        }
+        pthread_mutex_unlock(&threads);
+    }
 }
 
 int main()
 {
-    pthread_t a[N];
-    printf("Meu id: %lu\n", pthread_self());
+    printf("ID: %lu\n", pthread_self());
     int i;
     int *id;
     for (i = 0; i < N; i++)
@@ -24,11 +40,6 @@ int main()
         id = (int *)malloc(sizeof(int));
         *id = i;
         pthread_create(&a[i], NULL, pthread_func, (void *)(id));
-    }
-    while (counter < 1000)
-    {
-        counter++;
-        printf("A pthread %d incrementou counter para %d\n", *id, counter);
     }
     for (i = 0; i < N; i++)
     {
