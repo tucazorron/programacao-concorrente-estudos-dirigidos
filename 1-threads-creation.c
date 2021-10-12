d06546f6aae6052609faf329901511a083fe485a
artur zorron <tucazorron@gmail.com>
2021-10-11 23:31:49 -0300

    update 1

--- 1-threads-creation.c
+++ 1-threads-creation.c
@@ -1,50 +1,74 @@
+#include "pthread.h"
 #include "stdio.h"
 #include "stdlib.h"
-#include "pthread.h"
 #include "unistd.h"
 
-#define N 10
+// numero de threads
+#define number_of_threads 10
 
-pthread_mutex_t threads = PTHREAD_MUTEX_INITIALIZER;
-pthread_t a[N];
+// cria mutex
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+// contador
 int counter = 0;
-int flag = 1;
 
-void *pthread_func(void *id)
+// funcao de contador
+void *counter_function(void *arg)
 {
-    printf("\nTHREAD  =  %02d | CRIADA\n\n", *(int *)(id));
+    // trata o id da thread
+    int id = *(int *)(arg);
 
-    while (flag)
+    // cria thread
+    printf("thread [%02d] >> criada\n", id);
+
+    // loop infinito
+    while (1)
     {
-        pthread_mutex_lock(&threads);
-        if (counter < 1000)
+        // da lock
+        pthread_mutex_lock(&lock);
+
+        if (counter < 100)
         {
+            // mostra o contador
+            printf("thread [%02d] >> soma [%04d]\n", id, counter);
+
+            // incrementa contador
             counter++;
-            printf("COUNTER =  %04d | THREAD = %02d\n", counter, *(int *)(id));
         }
         else
         {
-            flag = 0;
+            // da unlock
+            pthread_mutex_unlock(&lock);
+
+            break;
         }
-        pthread_mutex_unlock(&threads);
+
+        // da unlock
+        pthread_mutex_unlock(&lock);
     }
 }
 
 int main()
 {
-    printf("ID: %lu\n", pthread_self());
+    // cria threads
+    pthread_t threads[number_of_threads];
+
     int i;
     int *id;
-    for (i = 0; i < N; i++)
+
+    for (i = 0; i < number_of_threads; i++)
     {
         id = (int *)malloc(sizeof(int));
         *id = i;
-        pthread_create(&a[i], NULL, pthread_func, (void *)(id));
+
+        // cria threads na funcao de contador
+        pthread_create(&threads[i], NULL, counter_function, (void *)(id));
     }
-    for (i = 0; i < N; i++)
+
+    for (i = 0; i < number_of_threads; i++)
     {
-        pthread_join(a[i], NULL);
+        // junta as threads
+        pthread_join(threads[i], NULL);
     }
-    printf("TERMINANDO\n");
     return 0;
 }
